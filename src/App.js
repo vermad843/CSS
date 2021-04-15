a71cec3236a3a7892043ed1354b7535ac886577a
vermad843 <vishnuverma7524@gmail.com>
2021-04-15 10:48:05 +0530

    opacity

--- src/App.js
+++ src/App.js
@@ -23,6 +23,11 @@ function App() {
              />
              <h1>Image</h1>
         </div>
+        <div className = "searchResult">
+           <img
+             src = "http://media.cntraveler.com/photos/5db1d0dd11c1e500092e7133/master/pass/airbnb-ski-aspen-28328347.jpg"
+              />
+        </div>
     </div>
   );
 }
